abed8dd600916cdb8f610b6e4e5bded8fd1a5838
PascalSoentgerath <139318561+PascalSoentgerath@users.noreply.github.com>
2023-12-07 12:36:16 +0100

    exchanged object 'game' instead of function 'game' to train how to use objects

--- script.js
+++ script.js
@@ -1,98 +1,120 @@
 
 
-// Hat eine der beiden Variablen 5 erreicht, gewinnt die jeweilige Seite,
-// der Score wird resettet,
-// und das Spiel startet neu
-// Zudem soll nach jeder Runde ein STRING ausgegeben werden welcher den Spielstand anzeigt
+const game = {
+    playerScore: 0,
+    computerScore: 0,
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
+    start() {
+        for (let i = 0; this.playerScore < 5 && this.computerScore < 5; i++) {
+            const selections = this.chooseOption()
 
-    for (let i = 0; playerScore < 5 && computerScore < 5; i++) {
-        const playerSelection = prompt("Choose Rock, Paper or Scissor").toLowerCase()
-        const computerSelection = getComputerChoice()
-        const result = playRound(playerSelection, computerSelection)
+            const result = this.playRound(selections[0], selections[1])
+            this.showResult(result)
+            this.checkScore()
+        }
+    },
 
+    chooseOption() {
+        let playerSelection = prompt("Choose Rock, Paper or Scissor").toLowerCase()
+        let computerSelection = this.getComputerChoice()
+        return [playerSelection, computerSelection]
+    },
+
+    getComputerChoice() {
+        const randomNumber =  Math.floor(Math.random() * 3) + 1
+    
+        switch (randomNumber) {
+            case 1: return "rock"
+                    break;
+            case 2: return "paper"
+                    break;
+            default: return "scissor"
+        }
+    },
+
+    playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "It's a tie, try again!"
+        } else {
+            if (playerSelection === "rock") {
+                if (computerSelection === "paper") {
+                    return "You lose! Paper beats Rock"
+                } else if (computerSelection === "scissor") {
+                    return "You win! Rock beats Scissor"
+                }
+            }
+    
+            if (playerSelection === "paper") {
+                if (computerSelection === "scissor") {
+                    return "You lose! Scissor beats Paper"
+                } else if (computerSelection === "rock") {
+                    return "You win! Paper beats rock"
+                }
+            }
+    
+            if (playerSelection === "scissor") {
+                if (computerSelection === "rock") {
+                    return "You lose! Rock beats Scissor"
+                } else if (computerSelection === "paper") {
+                    return "You win! Scissor beats Paper"
+                }
+            }
+        }
+    
+        alert("Please type in 'Rock', 'Paper' or 'Scissor ")
+        },
+
+    showResult(result) {
         if (result === undefined) {
-            continue
+            return
         }
         if (result.includes("You win")) {
-            playerScore++
+            this.updateScore("player")
             console.log(result)
-            updateScore(playerScore, computerScore)
         }
         if (result.includes("You lose")) {
-            computerScore++
+            this.updateScore("computer")
             console.log(result)
-            updateScore(playerScore, computerScore)
         }
         if (result.includes("It's a tie")) {
             console.log(result)
             alert("Tie!")
         }
-    }
+    },
 
-    if (playerScore === 5) {
-        alert("Congratulations! You Won")
-        if (confirm("Want to play again?")) {
-            game()
+    updateScore(winner) {
+        if (winner === "player") {
+            this.playerScore = this.playerScore + 1
+            alert(`${this.playerScore} - ${this.computerScore}`)
         }
-    }
-    if (computerScore === 5) {
-        alert("Unlucky! You lose")
-        if (confirm("Want to play again?")) {
-            game()
+        if (winner === "computer") {
+            this.computerScore = this.computerScore + 1
+            alert(`${this.playerScore} - ${this.computerScore}`)
         }
-    }
-}
+    },
 
-function getComputerChoice() {
-    const randomNumber =  Math.floor(Math.random() * 3) + 1
-
-    switch (randomNumber) {
-        case 1: return "rock"
-                break;
-        case 2: return "paper"
-                break;
-        default: return "scissor"
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a tie, try again!"
-    } else {
-        if (playerSelection === "rock") {
-            if (computerSelection === "paper") {
-                return "You lose! Paper beats Rock"
-            } else if (computerSelection === "scissor") {
-                return "You win! Rock beats Scissor"
+    checkScore() {
+        if (this.playerScore === 5) {
+            this.resetScore()
+            alert("Congratulations! You Won")
+            if (confirm("Want to play again?")) {
+                this.start()
             }
         }
-
-        if (playerSelection === "paper") {
-            if (computerSelection === "scissor") {
-                return "You lose! Scissor beats Paper"
-            } else if (computerSelection === "rock") {
-                return "You win! Paper beats rock"
+        if (this.computerScore === 5) {
+            this.resetScore()
+            alert("Unlucky! You lose")
+            if (confirm("Want to play again?")) {
+                this.start()
             }
         }
+                    
+    },
 
-        if (playerSelection === "scissor") {
-            if (computerSelection === "rock") {
-                return "You lose! Rock beats Scissor"
-            } else if (computerSelection === "paper") {
-                return "You win! Scissor beats Paper"
-            }
-        }
+    resetScore() {
+        this.playerScore = 0
+        this.computerScore = 0
     }
-
-    alert("Please type in 'Rock', 'Paper' or 'Scissor ")
 }
 
-function updateScore(player, computer) {
-    alert(`${player} - ${computer}`)
-}
-
-game()
+game.start()
